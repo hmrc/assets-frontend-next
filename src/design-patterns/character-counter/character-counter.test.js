9d50291b9a001036dbe1a792fc1041f1c1268e74
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-12-14 10:49:13 +0000

    code style tweeks

--- src/design-patterns/character-counter/character-counter.test.js
+++ src/design-patterns/character-counter/character-counter.test.js
@@ -1,10 +1,9 @@
 import test from 'ava'
-import nunjucks from 'nunjucks'
 import characterCounter from './character-counter.html'
 import renderTemplate from '../../common/utils/renderTemplate'
 
-test("date of birth should exist", t => {
-  template = renderTemplate(characterCounter)
+test('date of birth should exist', t => {
+  const template = renderTemplate(characterCounter)
 
   document.body.innerHTML += template
 
--- src/design-patterns/date-of-birth/date-of-birth.test.js
+++ src/design-patterns/date-of-birth/date-of-birth.test.js
@@ -1,10 +1,9 @@
 import test from 'ava'
-import nunjucks from 'nunjucks'
 import dateOfBirth from './date-of-birth.html'
 import renderTemplate from '../../common/utils/renderTemplate'
 
-test("date of birth should exist", t => {
-  template = renderTemplate(dateOfBirth)
+test('date of birth should exist', t => {
+  const template = renderTemplate(dateOfBirth)
 
   document.body.innerHTML += template
 
